2b5d57a2e04403ba44c7641ae09bbda2df5f0feb
Charles de Dreuille <charles.dedreuille@gmail.com>
2022-11-03 09:29:53 +0000

    Fix viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
           content="Charles de Dreuille is a digital product enthousiast from London — Connect design and engineering for humans of this world. For the past 12 years I had the chance to work for companies like Meta, Christian Louboutin, Deliveroo, Soho House and a handful of entrepreneurs delivering delightful experiences for their users."
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className="bg-black sm:bg-red">{children}</body>
     </html>
