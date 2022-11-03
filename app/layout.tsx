5632a574056224149969c23a59666012d07ff689
Charles de Dreuille <charles.dedreuille@gmail.com>
2022-11-03 14:19:37 +0000

    Add subset to font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "@next/font/google";
 
-const inter = Inter();
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
