8a78d73ff1a03d703f14664e35d57eeeb8735527
Charles de Dreuille <charles.dedreuille@gmail.com>
2022-11-10 15:05:25 +0000

    Improve cursor

--- app/components/cursor.tsx
+++ app/components/cursor.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
 import useMousePosition from "../hooks/useMousePosition";
 
 export const Cursor = () => {
   const positionRef = useRef(null);
   const { x, y } = useMousePosition();
-  const posX = x || 0;
-  const posY = y || 0;
+
+  useEffect(() => {
+    console.log("x", x);
+    console.log("y", y);
+  }, [x, y]);
+
+  if (x === null || y === null) return null;
 
   return (
-    <div
-      className="w-10 h-10 fixed bg-[#ff4500] z-[999] rounded-full mix-blend-difference pointer-events-none"
-      ref={positionRef}
-      style={{ left: `${posX - 20}px`, top: `${posY - 20}px` }}
-    />
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ x: x - 16, y: y - 16, opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.16, 0.6, 0.4, 1] }}
+        className="w-8 h-8 fixed bg-[#ff4500] z-[999] rounded-full mix-blend-difference pointer-events-none"
+        ref={positionRef}
+      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ x: x - 32, y: y - 32, opacity: 1 }}
+        transition={{ duration: 1, ease: [0.16, 0.6, 0.4, 1] }}
+        className="w-16 h-16 fixed z-[999] rounded-full mix-blend-difference pointer-events-none border border-[#ff4500]"
+        ref={positionRef}
+      />
+    </>
   );
 };
--- app/hooks/useMousePosition.tsx
+++ app/hooks/useMousePosition.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
 const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = React.useState({
+  const [mousePosition, setMousePosition] = useState<{
+    x: null | number;
+    y: null | number;
+  }>({
     x: null,
     y: null,
   });
-  React.useEffect(() => {
+
+  useEffect(() => {
     const updateMousePosition = (ev: { clientX: any; clientY: any }) => {
       setMousePosition({ x: ev.clientX, y: ev.clientY });
     };
+
     window.addEventListener("mousemove", updateMousePosition);
+
     return () => {
       window.removeEventListener("mousemove", updateMousePosition);
     };
   }, []);
+
+  useEffect(() => {
+    if (window.innerWidth && window.innerHeight)
+      setMousePosition({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  }, []);
+
   return mousePosition;
 };
+
 export default useMousePosition;
