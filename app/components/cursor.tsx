69c7a4f66ecac8f83c9d17675473a1cb2bbf678f
Charles de Dreuille <charles.dedreuille@gmail.com>
2022-11-10 15:36:19 +0000

    Improve cursor speed

--- app/components/cursor.tsx
+++ app/components/cursor.tsx
@@ -20,14 +20,14 @@ export const Cursor = () => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ x: x - 16, y: y - 16, opacity: 1 }}
-        transition={{ duration: 0.4, ease: [0.16, 0.6, 0.4, 1] }}
+        transition={{ duration: 0, ease: [0.16, 0.6, 0.4, 1] }}
         className="w-8 h-8 fixed bg-[#ff4500] z-[999] rounded-full mix-blend-difference pointer-events-none"
         ref={positionRef}
       />
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ x: x - 32, y: y - 32, opacity: 1 }}
-        transition={{ duration: 1, ease: [0.16, 0.6, 0.4, 1] }}
+        transition={{ duration: 0.6, ease: [0.16, 0.6, 0.4, 1] }}
         className="w-16 h-16 fixed z-[999] rounded-full mix-blend-difference pointer-events-none border border-[#ff4500]"
         ref={positionRef}
       />
