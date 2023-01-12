b470e37da11b7dced84da78ba431ee61489edf9d
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-01-12 20:37:58 +0000

    Update old-project.tsx

--- components/old-project.tsx
+++ components/old-project.tsx
@@ -28,64 +28,66 @@ export const Project: FC<Props> = ({ project }) => {
   const opacityText = useTransform(scrollYProgress, [1, 0.9], [0, 1]);
   const opacityLine = useTransform(scrollYProgress, [1, 0.99], [0, 1]);
 
-  return (
-    <div className="relative" ref={ref}>
-      <div id={project.slug} />
-      <motion.div
-        className="sticky top-0 bg-cream border-t border-black z-30"
-        style={{ borderColor: colorBorder }}
-      >
-        <motion.div
-          initial={{ opacity: 0 }}
-          className="w-full h-2 bg-cream absolute -top-1 left-0"
-          style={{ opacity: opacityLine }}
-        />
-        <div className="flex justify-between items-center py-2 bg-cream">
-          <div className="text-base">{project.published_at.slice(0, 4)}</div>
-          {width && width > 768 && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              className="text-base"
-              style={{ opacity: opacityText }}
-            >
-              {project.client} - {project.name}
-            </motion.div>
-          )}
-          <div className="uppercase text-sm">{project.type}</div>
-        </div>
-      </motion.div>
-      <div className="pb-8 origin-top-left text-2xl sm:text-3xl">
-        {project.client} - {project.name}
-      </div>
-      <div className="max-w-6xl mb-12">{project.description}</div>
-      {/* {project.blocks?.map((block) => (
-        <div key={block._key} className="mb-8 flex flex-row gap-8">
-          {block.assets?.map((asset) => {
-            if (asset.type === "image") {
-              return (
-                <div key={asset._key}>
-                  {asset.url && (
-                    <Image
-                      src={`${asset.url}?w=1800`}
-                      width={asset.width}
-                      height={asset.height}
-                      alt="Image"
-                    />
-                  )}
-                </div>
-              );
-            }
-            if (asset.type === "mux") {
-              return <Video asset={asset} key={asset._key} />;
-            }
-            return null;
-          })}
-        </div>
-      ))}
-      {project.blocks === null && (
-        <div className="w-full h-[800px] bg-white rounded-md" />
-      )} */}
-      <div className="w-full h-[100px] bg-cream" />
-    </div>
+  return <></>
+
+  // return (
+  //   <div className="relative" ref={ref}>
+  //     <div id={project.slug} />
+  //     <motion.div
+  //       className="sticky top-0 bg-cream border-t border-black z-30"
+  //       style={{ borderColor: colorBorder }}
+  //     >
+  //       <motion.div
+  //         initial={{ opacity: 0 }}
+  //         className="w-full h-2 bg-cream absolute -top-1 left-0"
+  //         style={{ opacity: opacityLine }}
+  //       />
+  //       <div className="flex justify-between items-center py-2 bg-cream">
+  //         <div className="text-base">{project.published_at.slice(0, 4)}</div>
+  //         {width && width > 768 && (
+  //           <motion.div
+  //             initial={{ opacity: 0 }}
+  //             className="text-base"
+  //             style={{ opacity: opacityText }}
+  //           >
+  //             {project.client} - {project.name}
+  //           </motion.div>
+  //         )}
+  //         <div className="uppercase text-sm">{project.type}</div>
+  //       </div>
+  //     </motion.div>
+  //     <div className="pb-8 origin-top-left text-2xl sm:text-3xl">
+  //       {project.client} - {project.name}
+  //     </div>
+  //     <div className="max-w-6xl mb-12">{project.description}</div>
+  //     {/* {project.blocks?.map((block) => (
+  //       <div key={block._key} className="mb-8 flex flex-row gap-8">
+  //         {block.assets?.map((asset) => {
+  //           if (asset.type === "image") {
+  //             return (
+  //               <div key={asset._key}>
+  //                 {asset.url && (
+  //                   <Image
+  //                     src={`${asset.url}?w=1800`}
+  //                     width={asset.width}
+  //                     height={asset.height}
+  //                     alt="Image"
+  //                   />
+  //                 )}
+  //               </div>
+  //             );
+  //           }
+  //           if (asset.type === "mux") {
+  //             return <Video asset={asset} key={asset._key} />;
+  //           }
+  //           return null;
+  //         })}
+  //       </div>
+  //     ))}
+  //     {project.blocks === null && (
+  //       <div className="w-full h-[800px] bg-white rounded-md" />
+  //     )} */}
+  //     <div className="w-full h-[100px] bg-cream" />
+  //   </div>
   );
 };
